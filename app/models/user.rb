dc8a79c8f4daa80ad4f2cff0bc1eb058430b1496
zainabfarooq26 <bitf20m026@pucit.edu.pk>
2025-02-01 20:02:01 +0500

    improve the client and admin dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :active, inclusion: { in: [true, false] }
-
   has_one_attached :profile_photo
   has_one :profile, dependent: :destroy
   has_many :clients, dependent: :destroy 
-
   def manager?
     is_manager == true
   end
-
   def locked?
     !self.active? # Returns true if user is inactive (locked)
   end  
-
   private
-
   def create_profile
     self.create_profile!(
       first_name: '',
