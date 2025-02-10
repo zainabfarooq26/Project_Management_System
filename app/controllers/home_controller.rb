baee6417abae1fc297be028018e660316472cc94
zainabfarooq26 <bitf20m026@pucit.edu.pk>
2025-02-10 21:15:48 +0500

    update the graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     def index
     @top_projects = Project.order(total_earnings: :desc).limit(5)
     @bottom_projects = Project.order(total_earnings: :asc).limit(5)
-    @monthly_earnings = Payment.group_by_month(:paid_on).sum(:amount)
+    @monthly_earnings = Payment.where(status: 'paid').group_by_month(:paid_on).sum(:amount)
     @monthly_hours = TimeLog.group_by_month(:created_at).sum(:hours)
 
     end
