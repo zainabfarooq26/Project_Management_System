377a51369df0bbbe0e9e56bed2c7368f7a7c144a
zainabfarooq26 <bitf20m026@pucit.edu.pk>
2025-02-12 16:18:57 +0500

    remove the duplicate migration files

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,11 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @users = User.where(admin: false) 
+    @users = User.where.not(admin: true)  
   end
   private
   def authenticate_admin!
     redirect_to root_path, alert: "Access Denied!" unless current_user.admin?
   end
 end
+      
